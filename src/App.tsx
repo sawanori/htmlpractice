4e5e68681ca4d1b641f8221124ad09da3bf09739
noritakasawada <snp.inc.info@gmail.com>
2022-07-27 10:06:35 +0900

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import style from './App.module.scss';
+import { Test } from './component/test';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.App}>
+      App
+      <Test />
+      <h1>test</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
